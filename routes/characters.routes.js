ebd89549bb8eb0fa1772f8b13dd8742b33075f82
afisman <afisman@gmail.com>
2022-06-29 20:37:57 +0200

    Almost finished, was not able to finish setting debt in creatin

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,27 +1,98 @@
 const router = require("express").Router();
-const axios = require("axios");
+
+const characterService = require('./../services/character.services')
 
 /* GET home page */
 router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+    characterService
+        .getAllCharacters()
+        .then(response => {
+            const characters = response.data
+            res.render('characters/list-characters', { characters })
+        })
+        .catch(err => console.log(err))
 });
 
 
-router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
+router.get("/characters/:char_id", (req, res, next) => {
+    const { char_id } = req.params
+
+    characterService
+        .getOneCharacter(char_id)
+        .then(response => {
+            const character = response.data
+            res.render('characters/details-character', { character })
+        })
+        .catch(err => console.log(err))
+
+})
+
+router.get("/create", (req, res, next) => {
+
+    res.render('characters/create-character')
+
+})
+
+router.post("/create", (req, res, next) => {
+    const { name, occupation, weapon } = req.body
+    // const { debt } = req.body.debt ? true : false
+    // if (req.body.debt) {                                     //Tried to set debt in creatin, was not able to
+    //     debt = true
+    // }
+
+    const newCharacter = { name, occupation, weapon, debt }
+    console.log(newCharacter)
+
+    characterService
+        .saveCharacter(newCharacter)
+        .then(response => res.redirect('/characters'))
+        .catch(err => console.log(err))
+})
+
+router.get("/characters/edit/:id", (req, res, next) => {
+    const { id } = req.params
+
+    characterService
+        .getOneCharacter(id)
+        .then(response => {
+            const character = response.data
+            res.render('characters/edit-character', { character })
+        }
+        )
+        .catch(err => console.log(err))
+
+})
+
+router.post("/characters/edit/:id", (req, res, next) => {
+    const { id } = req.params
+    const { debt } = req.body.debt ? req.body.debt === true : req.body.debt === false
+
+    const { name, occupation, weapon } = req.body
+    const newCharData = { name, occupation, weapon, debt }
+
+    characterService
+        .editCharacter(id, newCharData)
+        .then(response => {
+
+            res.redirect('/characters')
+        }
+        )
+        .catch(err => console.log(err))
+
+})
+
+router.get("/characters/delete/:char_id", (req, res, next) => {
+    const { char_id } = req.params
+
+    characterService
+        .deleteCharacter(char_id)
+        .then(response => res.redirect('/characters'))
+        .catch(err => console.log(err))
+
+})
+
+
 
 module.exports = router;
 
 
-// https://ih-crud-api.herokuapp.com/characters
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,4 +5,7 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+const characterRouter = require('./characters.routes')
+router.use('/', characterRouter)
+
 module.exports = router;
