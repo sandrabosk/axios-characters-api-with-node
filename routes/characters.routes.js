b241cf486187ef670b4d11807e9966f9bbf0e293
Miguel A. Garcia <miguelangelgarcia994@gmail.com>
2022-11-16 20:04:23 +0100

    done

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const axios = require("axios");
 
+const charactersApi = require('./../services/characters-api-services')
+const api= new charactersApi()
+
 /* GET home page */
 router.get("/characters", (req, res, next) => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
@@ -11,6 +14,20 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.get("/characters/create", (req, res, next) =>
+{
+    res.render('characters/create-character')
+})
+
+router.post("/characters/create", (req, res, next) => {
+
+  const { name, occupation, weapon } = req.body
+
+  api
+    .createCharacter({ name, occupation, weapon })
+    .then(() => res.redirect('/characters'))
+    .catch(err => console.log(err))
+})
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
@@ -21,7 +38,42 @@ router.get("/characters/:id", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+router.get('/characters/:id/edit', (req, res, next) => {
+
+    const{id:character_Id}=req.params
+    
+api
+    .getOneCharacter(character_Id)
+    .then(response => res.render('characters/edit-character',{character:response.data}))
+    .catch(err => console.error(err))
+})
+
+router.post('/characters/:id/edit', (req, res, next) =>{
+
+const {id:character_Id} =req.params   
+const { name, weapon, occupation } = req.body
+    api
+        .editCharacter(character_Id, { name, weapon, occupation })
+        .then(res.redirect('/characters'))
+  .catch(err => console.log(err))
+
+})
+
+router.post('/characters/:id/delete', (req, res, next) => {
+
+  const {id: character_Id } = req.params
+
+  api
+    .deleteCharacter(character_Id)
+    .then(() => res.redirect('/characters'))
+    .catch(err => console.log(err))
+
+})
+
+
+
+
+
 module.exports = router;
 
 
-// https://ih-crud-api.herokuapp.com/characters
