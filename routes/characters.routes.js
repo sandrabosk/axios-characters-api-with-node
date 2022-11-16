e425732e008813b5c20f3f13989308e3f9a61c55
Carlos Bustos Ramiro <carlosbustosramiro@gmail.com>
2022-11-16 20:21:17 +0100

    done

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -4,21 +4,62 @@ const axios = require("axios");
 /* GET home page */
 router.get("/characters", (req, res, next) => {
     axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+        .then(responseFromAPI => {
+            res.render("characters/list-characters", { characters: responseFromAPI.data });
+        })
+        .catch(next)
 });
 
+router.get("/character/create", (req, res, next) => {
+    res.render('characters/create-character');
+});
 
 router.get("/characters/:id", (req, res, next) => {
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+        .then(responseFromAPI => {
+            res.render("characters/details-character", { character: responseFromAPI.data });
+        })
+        .catch(next)
+});
+
+router.get('/character/delete/:id', (req, res, next) => {
+    const { id } = req.params;
+    axios.delete(`https://ih-crud-api.herokuapp.com/characters/${id}`)
+        .then(() => {
+            res.redirect("/characters");
+        })
+        .catch(next);
+});
+
+router.get('/character/edit/:id', (req, res, next) => {
+    const { id } = req.params;
+    const debtArray = ["true", "false"];
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${id}`)
+        .then((responseFromAPI) => {
+            debtArray.map((debt, i, debts) => { if (String(debt) === String(responseFromAPI.data.debt)) { debts.splice(i, 1) } })
+            res.render("characters/edit-character", { character: responseFromAPI.data, debtArray });
+        })
+        .catch(next);
+});
+/* POST */
+router.post("/character/create", (req, res, next) => {
+    const { name, occupation, weapon, debt } = req.body;
+
+    axios.post(`https://ih-crud-api.herokuapp.com/characters`, { name, occupation, weapon, debt })
+        .then(responseFromAPI => {
+            res.render("characters/details-character", { character: responseFromAPI.data });
+        })
+        .catch(next)
+});
+
+router.post("/character/edit/:id", (req, res, next) => {
+    const { id } = req.params;
+    const { name, occupation, weapon, debt } = req.body;
+    axios.put(`https://ih-crud-api.herokuapp.com/characters/${id}`, { name, occupation, weapon, debt })
+        .then(responseFromAPI => {
+            res.render("characters/details-character", { character: responseFromAPI.data });
+        })
+        .catch(next)
 });
 
 module.exports = router;
