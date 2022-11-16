de45779e41021b6024ec4d7ac3ed4ee3d03f96f4
davidmunoz626 <115696661+davidmunoz626@users.noreply.github.com>
2022-11-16 19:19:15 +0100

    done

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+module.exports = app => {
+
+    const indexRouter = require("./index.routes")
+    app.use("/", indexRouter)
+
+
+
+    const charactersRouter = require("./characters.routes")
+    app.use("/characters", charactersRouter)
+
+
+}
