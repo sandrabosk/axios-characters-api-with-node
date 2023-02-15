359d2965ba7412af7d7c7e2d3f04f71fc347a3d5
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2023-02-15 22:15:46 +0100

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,15 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema(
+const characterSchema = new Schema(
   {
-    username: {
-      type: String,
-      // unique: true -> Ideally, should be unique, but its up to you
-    },
-    password: String,
+    name: String,
+    occupation: String,
+    weapon: String,
+    debt: Boolean
   },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
 );
 
-const User = model("User", userSchema);
+const Character = model("Character", characterSchema)
 
-module.exports = User;
+module.exports = Character
