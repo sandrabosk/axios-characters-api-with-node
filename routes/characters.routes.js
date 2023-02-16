df859cb96840c06ae9d02fa99a51015d582e5564
Bernardo Vera <bvera.ruiz@gmail.com>
2023-02-16 09:06:37 +0100

    done 40%

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,25 +1,42 @@
 const router = require("express").Router();
-const axios = require("axios");
+const axios = require("axios")
+
+const CharacterApi = require('../services/characters.services')
+
+const characterApi = new CharacterApi()
 
 /* GET home page */
-router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+router.get('/characters/list', (req, res, next) => {
+
+    characterApi
+        .getAll()
+        .then(all => res.render('characters/list-characters', { characters: all.data }))
+        .catch(err => next(err))
 });
 
 
-router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
+// router.get("/characters/:id", (req, res, next) => {
+//     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+//         .then(responseFromAPI => {
+//             // console.log("details: ", responseFromAPI.data)
+//             res.render("characters/details-character", { character: responseFromAPI.data });
+//         })
+//         .catch(err => console.error(err))
+// });
+
+router.get('/crear', (req, res, next) => {
+    res.render('characters/create-character')
+})
+
+router.post('/crear', (req, res, next) => {
+    const { name, occupation, weapon } = req.body
+
+    characterApi
+        .saveCharacter({ name, occupation, weapon })
+        .then(() => res.redirect('/list'))
+        .catch(err => next(err))
+})
+
 
 module.exports = router;
 
