334453ef610b53ab1266e4b01477d9613e535678
arturhonores <luis.arturhonores@gmail.com>
2023-05-10 23:41:46 +0200

    done

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,27 +1,76 @@
-const router = require("express").Router();
-const axios = require("axios");
+const express = require('express');
+const router = express.Router();
 
-/* GET home page */
-router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
+const charactersApiHandler = require('../services/characters-api.service');
 
 
-router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
 
-module.exports = router;
+router.get("/listado", (req, res, next) => {
+
+    charactersApiHandler
+        .getAllCharacters()
+        .then(response => res.render('characters/list', { characters: response.data }))
+        .catch(err => next(err))
+})
 
 
-// https://ih-crud-api.herokuapp.com/characters
+router.get("/detalles/:character_id", (req, res, next) => {
+
+    const { character_id } = req.params
+
+    charactersApiHandler
+        .getOneCharacter(character_id)
+        .then(response => res.render('characters/details', { character: response.data }))
+        .catch(err => next(err))
+})
+
+router.get("/crear", (req, res, next) => {
+    res.render('characters/create')
+})
+
+router.post("/crear", (req, res, next) => {
+
+    const { name, weapon, occupation } = req.body
+
+    charactersApiHandler
+        .saveCharacter({ name, weapon, occupation })
+        // .then(response => res.redirect(`/detalles/${response.data.id}`))
+        .then(response => res.redirect(`/listado`))
+        .catch(err => next(err))
+})
+
+
+router.get("/editar/:character_id", (req, res, next) => {
+
+    const { character_id } = req.params
+
+    charactersApiHandler
+        .getOneCharacter(character_id)
+        .then(response => res.render('characters/edit', { character: response.data }))
+        .catch(err => next(err))
+
+})
+
+
+router.post("/editar/:character_id", (req, res, next) => {
+
+    const { name, weapon, occupation } = req.body
+    const { character_id } = req.params
+
+    charactersApiHandler
+        .editCharacter(character_id, { name, weapon, occupation })
+        // .then(response => res.redirect(`personajes/detalles/${response.data.id}`))
+        .then(response => res.redirect(`/detalles/${response.data.id}`))
+        .catch(err => next(err))
+})
+
+router.post("/delete/:id", (req, res, next) => {
+    const { id } = req.params
+
+    charactersApiHandler
+        .deleteCharacter(id)
+        .then(response => res.redirect('/listado'))
+        .catch(err => next(err))
+
+})
+module.exports = router
---
+++ services/characters-api.service.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+
+class CharactersApiHandler {
+
+    constructor() {
+        this.axiosApp = axios.create({
+            baseURL: 'https://ih-crud-api.herokuapp.com'
+        })
+    }
+
+    getAllCharacters() {
+        return this.axiosApp.get('/characters')
+    }
+
+    getOneCharacter(characterId) {
+        return this.axiosApp.get(`/characters/${characterId}`)
+    }
+
+    saveCharacter(characterInfo) {
+        return this.axiosApp.post(`/characters`, characterInfo)
+    }
+
+    editCharacter(characterId, characterInfo) {
+        return this.axiosApp.put(`/characters/${characterId}`, characterInfo)
+    }
+
+    deleteCharacter(characterId) {
+        return this.axiosApp.delete(`/characters/${characterId}`)
+    }
+}
+
+const charactersApiHandler = new CharactersApiHandler()
+
+module.exports = charactersApiHandler
