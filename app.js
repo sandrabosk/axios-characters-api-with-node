7395d9245f089b3868d53f8ac7041ddad5c0841d
Astrid <anaguflo@gmail.com>
2023-05-10 21:15:53 +0200

    done(?)

--- app.js
+++ app.js
@@ -35,3 +35,79 @@ app.use("/", charactersRoutes);
 require("./error-handling")(app);
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const router = express.Router();
+
+// const charactersApiHandler = require('../services/characters-api.service');
+
+
+
+// router.get("/listado", (req, res, next) => {
+
+//     charactersApiHandler
+//         .getAllCharacters()
+//         .then(response => res.render('characters/list', { characters: response.data }))
+//         .catch(err => next(err))
+// })
+
+
+// router.get("/detalles/:character_id", (req, res, next) => {
+
+//     const { character_id } = req.params
+
+//     charactersApiHandler
+//         .getOneCharacter(character_id)
+//         .then(response => res.render('characters/details', { character: response.data }))
+//         .catch(err => next(err))
+
+
+// })
+
+// router.get("/crear", (req, res, next) => {
+//     res.render('characters/create')
+// })
+
+// router.post("/crear", (req, res, next) => {
+
+//     const { name, weapon, occupation } = req.body
+
+//     charactersApiHandler
+//         .saveCharacter({ name, weapon, occupation })
+//         .then(response => res.redirect(`/personajes/detalles/${response.data.id}`))
+//         .catch(err => next(err))
+// })
+
+
+// router.get("/editar/:character_id", (req, res, next) => {
+
+//     const { character_id } = req.params
+
+//     charactersApiHandler
+//         .getOneCharacter(character_id)
+//         .then(response => res.render('characters/edit', { character: response.data }))
+//         .catch(err => next(err))
+
+// })
+
+
+// router.post("/editar/:character_id", (req, res, next) => {
+
+//     const { name, weapon, occupation } = req.body
+//     const { character_id } = req.params
+
+//     charactersApiHandler
+//         .editCharacter(character_id, { name, weapon, occupation })
+//         .then(response => res.redirect(`/personajes/detalles/${response.data.id}`))
+//         .catch(err => next(err))
+// })
+
