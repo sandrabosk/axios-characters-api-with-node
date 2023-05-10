7a496ed9a24a4921382598554466fe05f3aa7424
Victor Villalba <villalbabesada@gmail.com>
2023-05-10 20:36:41 +0200

    first commit

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,27 +1,74 @@
 const router = require("express").Router();
 const axios = require("axios");
+const apiCharacters = require('../services/charact-api.service');
 
-/* GET home page */
-router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
+router.get("/characters-list", (req, res, next) => {
 
+    apiCharacters
+        .getAllCharacters()
+        .then(response => res.render('characters/list-characters', { characters: response.data }))
+        .catch(err => next(err))
+})
+
+router.get("/characters/create", (req, res, next) => {
+    res.render('characters/create-character')
+})
+
+router.post("/characters/create", (req, res, next) => {
+
+    const { name, weapon, debt, occupation } = req.body
+
+    apiCharacters
+        .saveCharacter({ name, weapon, debt, occupation })
+        .then(response => res.redirect(`/characters/${response.data.id}`))
+        .catch(err => next(err))
+})
 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
 
-module.exports = router;
+    const { id } = req.params
+
+    apiCharacters
+        .getOneCharacter(id)
+        .then(response => res.render('characters/details-character', { character: response.data }))
+        .catch(err => next(err))
+
+})
 
 
-// https://ih-crud-api.herokuapp.com/characters
+router.get("/characters/:id/edit", (req, res, next) => {
+
+    const { id } = req.params
+
+    apiCharacters
+        .getOneCharacter(id)
+        .then(response => res.render('characters/edit-character', { character: response.data }))
+        .catch(err => next(err))
+
+})
+
+
+router.post("/characters/:id/edit", (req, res, next) => {
+
+    const { name, weapon, debt, occupation } = req.body
+    const { id } = req.params
+
+    apiCharacters
+        .editCharacter(id, { name, weapon, debt, occupation })
+        .then(response => res.redirect(`/characters/${response.data.id}`))
+        .catch(err => next(err))
+})
+
+router.post("/characters/:id/delete", (req, res, next) => {
+    const { id } = req.params
+
+    apiCharacters
+        .deleteCharacter(id)
+        .then(() => res.redirect(`/characters-list`))
+        .catch(err => next(err))
+
+
+})
+
+
+module.exports = router
