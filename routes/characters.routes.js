e83a391224b909f2c53077eeb8046b2428ba6acd
Ricardo Navarro <ranr279@hotmail.com>
2023-05-10 21:27:57 +0200

    doneeeeeeee

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,27 +1,72 @@
 const router = require("express").Router();
-const axios = require("axios");
+const miMamaMePega = require('../services/api.services')
 
-/* GET home page */
+// get all pajuos
 router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+    miMamaMePega
+        .todosLosPajuos()
+        .then(responceFromAPI => res.render('characters/list-characters', { characters: responceFromAPI.data }))
+        .catch(err => console.error(err))
 });
 
 
+//crear al mas mamaguevo
+router.get("/create", (req, res, next) => {
+    res.render("characters/create")
+})
+
+router.post("/create", (req, res, next) => {
+    const { name, weapon, occupation, debt } = req.body
+
+    miMamaMePega
+        .crearMamaguevo({ name, weapon, occupation, debt })
+        .then(response => res.redirect(`/characters/${response.data.id}`))
+        .catch(err => console.error(err))
+})
+
+//detalles del pajuo 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+    const { id } = req.params
+    miMamaMePega
+        .idDelPajuo(id)
+        .then(responceFromAPI =>
+            res.render('characters/details-character', { characters: responceFromAPI.data }))
+        .catch(err => console.error(err))
+
 });
+
+//editar al maldito
+router.get("/edit/:id", (req, res, next) => {
+    const { id } = req.params
+
+    miMamaMePega
+        .idDelPajuo(id)
+        .then(responceFromAPI =>
+            res.render('characters/edit', { characters: responceFromAPI.data }))
+        .catch(err => console.error(err))
+})
+
+router.post("/edit/:id", (req, res, next) => {
+    const { id } = req.params
+    const { name, weapon, occupation, debt } = req.body
+
+    miMamaMePega
+        .editarMamaguevo(id, { name, weapon, occupation, debt })
+        .then(response => res.redirect(`/characters/${response.data.id}`))
+        .catch(err => console.error(err))
+
+})
+
+// borrar maldito
+router.post("/delete/:id", (req, res, next) => {
+    const { id } = req.params
+
+    miMamaMePega
+        .eliminarMamaguevo(id)
+        .then(() => res.redirect("/characters"))
+        .catch(err => console.log(err))
+
+})
 
 module.exports = router;
-
-
-// https://ih-crud-api.herokuapp.com/characters
+    // https://ih-crud-api.herokuapp.com/characters
