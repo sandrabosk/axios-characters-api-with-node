8f8679623f2caa3c7e1b88ba99c0311a725eec7a
cadelme <carlosd@MacBook-Pro-de-Carlos-2.local>
2023-08-09 18:00:44 +0200

    lab character cambios

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
-const axios = require("axios");
+ const axios = require("axios");
+
+const charactersApi = require('../services/characters.service')
 
 /* GET home page */
 router.get("/characters", (req, res, next) => {
@@ -11,8 +13,70 @@ router.get("/characters", (req, res, next) => {
     .catch(err => console.error(err))
 });
 
+// crear personaje
+
+router.get('/characters/create', (req, res) => {
+    res.render('characters/create-character')
+})
+
+
+router.post('/characters/create', (req, res, next) => {
+
+    const { name, weapon, occupation } = req.body
+
+    const newCharacter = { name, weapon, occupation }
+
+    charactersApi
+        .saveCharacter(newCharacter)
+        .then(() => res.redirect('/characters'))
+        .catch(err => next(err))
+})
+
+
+// editar personaje
+
+router.get('/characters/:id/edit', (req, res, next) => {
+
+    const { id: character_id } = req.params
+
+    charactersApi
+        .getOneCharacter(character_id)
+        .then(response => res.render('characters/edit-character', { character: response.data }))
+        .catch(err => next(err))
+})
+
+
+router.post('/characters/:id/update', (req, res, next) => {
+
+    const { id: character_id } = req.params
+    const { name, weapon, occupation } = req.body
+
+    const newCharacter = { name, weapon, occupation }
+
+    charactersApi
+        .editCharacter(character_id, newCharacter)
+        .then(() => res.redirect(`/characters/${character_id}`))
+        .catch(err => next(err))
+})
+
+
+// Eliminar Personaje
+
+router.post('/characters/:id/delete', (req, res, next) =>{
+
+    const { id: character_id } = req.params
+
+    charactersApi
+        .deleteCharacter(character_id)
+        .then(() => res.redirect('/characters'))
+        .catch(err => next(err))
+})
+
+
+
 
 router.get("/characters/:id", (req, res, next) => {
+    console.log("entro en segundo endpoint")
     axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
     .then(responseFromAPI => {
         // console.log("details: ", responseFromAPI.data)
@@ -20,6 +84,10 @@ router.get("/characters/:id", (req, res, next) => {
     })
     .catch(err => console.error(err))
 });
+
+
+
+
 
 module.exports = router;
 
