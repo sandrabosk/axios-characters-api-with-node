a33dd1c481144dc7076659b2fdc40472dad29e25
FERGAZA <fernandomgzapata@gmail.com>
2023-08-09 18:36:03 +0200

    task done

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -1,25 +1,82 @@
+const charactersApi = require("../services/characters.services");
+
 const router = require("express").Router();
-const axios = require("axios");
 
 /* GET home page */
 router.get("/characters", (req, res, next) => {
-    axios.get("https://ih-crud-api.herokuapp.com/characters")
-    .then(responseFromAPI => {
-        // console.log(responseFromAPI)
-        res.render("characters/list-characters", { characters: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
-});
+    charactersApi
+        .getAllCharacters()
+        .then(response => res.render('characters/list-characters', { characters: response.data }))
+        .catch(err => next(err))
+})
+
+
+router.get("/characters/create", (req, res, next) => {
+
+    res.render('characters/create-character')
+})
 
 
 router.get("/characters/:id", (req, res, next) => {
-    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
-    .then(responseFromAPI => {
-        // console.log("details: ", responseFromAPI.data)
-        res.render("characters/details-character", { character: responseFromAPI.data });
-    })
-    .catch(err => console.error(err))
+    const { id: character_id } = req.params
+
+    charactersApi
+        .getOneCharacter(character_id)
+        .then(response => res.render('characters/details-character', { character: response.data }))
+        .catch(err => next(err))
+
 });
+
+router.post('/characters/create', (req, res, next) => {
+
+    const { name, weapon, occupation } = req.body
+
+    const newCharacter = { name, weapon, occupation }
+
+    charactersApi
+        .saveCharacter(newCharacter)
+        .then(() => res.redirect('/characters'))
+        .catch(err => next(err))
+
+})
+
+router.get('/characters/:id/edit', (req, res, next) => {
+
+    const { id: character_id } = req.params
+
+    charactersApi
+        .getOneCharacter(character_id)
+        .then(response => res.render('characters/edit-character', { character: response.data }))
+        .catch(err => next(err))
+
+})
+
+router.post('/characters/:id/update', (req, res, next) => {
+
+    const { id: character_id } = req.params
+
+    const { name, weapon, occupation } = req.body
+
+    const newCharacter = { name, weapon, occupation }
+
+    charactersApi
+        .editCharacter(character_id, newCharacter)
+        .then(() => res.redirect('/characters'))
+        .catch(err => next(err))
+
+})
+
+
+router.post('/characters/:id/delete', (req, res, next) => {
+
+    const { id: character_id } = req.params
+
+    charactersApi
+        .deleteCharacter(character_id)
+        .then(() => res.redirect('/characters'))
+        .catch(err => next(err))
+})
+
 
 module.exports = router;
 
