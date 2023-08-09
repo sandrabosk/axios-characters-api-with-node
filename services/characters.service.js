cad788b2d2900521b9e740b81b24ccfbaaca5355
H1V35 <higesjorge@gmail.com>
2023-08-09 22:44:47 +0200

    Done

---
+++ services/characters.service.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+
+class CharactersApiHandler {
+  constructor() {
+    this.axiosApp = axios.create({
+      baseURL: 'https://ih-crud-api.herokuapp.com/characters'
+    })
+  }
+
+  getAllCharacters() {
+    return this.axiosApp.get('/')
+  }
+
+  getOneCharacter(character_id) {
+    return this.axiosApp.get(`/${character_id}`)
+  }
+
+  createCharacter(character_data) {
+    return this.axiosApp.post('/', character_data)
+  }
+
+  editCharacter(character_id, character_data) {
+    return this.axiosApp.put(`/${character_id}`, character_data)
+  }
+
+  deleteCharacter(character_id) {
+    return this.axiosApp.delete(`/${character_id}`)
+  }
+}
+
+const charactersApi = new CharactersApiHandler()
+
+module.exports = charactersApi
