f6c1279c391f5c06e2c2415c78a2067b5ddc1b02
SandraPerez <iamsandraperez.sndr@gmail.com>
2023-11-08 21:43:25 +0100

    done

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 // https://www.npmjs.com/package/dotenv
 require("dotenv/config");
 
-// ℹ️ Connects to the database
-require("./db");
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
