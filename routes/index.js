56326c9bb9734927b2a5810833690f5b52977bf9
alvaro sanchez <alvaro_piro_@hotmail.com>
2023-11-09 10:04:49 +0100

    finish

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+module.exports = app => {
+
+    const indexRoutes = require('./index.routes')
+    app.use('/', indexRoutes)
+
+    const charactersRoutes = require('./characters.routes')
+    app.use('/personajes', charactersRoutes)
+
+}
